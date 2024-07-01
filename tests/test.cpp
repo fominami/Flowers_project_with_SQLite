889a5b5ce0977ab2f07c16f624b6e3f7d62d8472
Maria Fomina <6002maria@gmail.com>
2024-07-01 19:57:47 +0300

    add lab2

---
+++ tests/test.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "v_t.h"
+TEST(MyValidator,validate_login) {
+	MyValidator v;
+  EXPECT_TRUE(v.validate_login(std::string("vika66")));
+  EXPECT_FALSE(v.validate_login(std::string("Vika66")));
+}
+TEST(MyValidator, validate_pass) {
+	MyValidator vo;
+	EXPECT_TRUE(vo.validate_login(std::string("Vi123456")));
+	EXPECT_FALSE(vo.validate_login(std::string("1234")));
+}
